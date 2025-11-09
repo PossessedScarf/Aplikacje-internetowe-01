795c621ed9dbc4904571388d740597fd72343fd5
PossessedScarf <sub01empty@gmail.com>
2025-11-09 11:18:17 +0100

    Local leaflet Test - Glitch Map

---
+++ AI1_lab03/script.js
@@ -0,0 +1,12 @@
+// •	pojedyncza strona HTML ze skryptem ładowanym z zewnętrznego pliku JS
+// •	pobranie zgody na lokalizację
+// •	pobranie zgody na wyświetlanie powiadomień
+// •	okno dynamicznej mapy (powiększanie/pomniejszanie, przesuwanie)
+// •	przycisk „Moja lokalizacja” – wyświetla współrzędne oraz oznacza na mapie
+// •	przycisk „Pobierz mapę” – eksportuje mapę w postaci rastrowej
+// •	mapa rastrowa zostaje podzielona na 16 elementów i wymieszana; elementy rozrzucone na „stole”
+// •	użycie mechanizmu drag & drop do przemieszczania elementów na „stole”
+// •	w tle weryfikacja czy element ustawiony na swoim miejscu
+// •	w momencie ustawienia wszystkich elementów na swoim miejscu – wyświetlenie notyfikacji systemowej (https://developer.mozilla.org/en-US/docs/Web/API/Notification)
+// Wideo z omówieniem działającej aplikacji: https://www.youtube.com/watch?v=Peb_mgDTY0s.
+
